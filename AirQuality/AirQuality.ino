9de8b0c8c4e92f8e4506bab67174cc5dbfa7cf5f
James Hansen <dochansen@gmail.com>
2019-04-19 13:21:30 -0400

    V1 changes to the original from instructables

--- AirQuality/AirQuality.ino
+++ AirQuality/AirQuality.ino
@@ -1,4 +1,6 @@
-#include <Preferences.h>
+#include <ThingSpeak.h>
+#include "secrets.h"
+
 #include <WiFi.h>
 #include <Wire.h>
 #include <SFE_MicroOLED.h>
@@ -19,9 +21,11 @@
 #define RFID_TIME_BYTES 4
 #define RFID_TOTAL_BYTES 10
 
-Preferences preferences;
-
 WiFiServer server(80);
+WiFiClient wifiClient;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char* myWriteAPIKey = SECRET_WRITE_APIKEY;
 
 MicroOLED oled(OLED_RESET_PIN, OLED_JUMPER_MODE);
 
@@ -68,17 +72,15 @@ void setup() {
 
   Serial.println("");
   
-  preferences.begin("pda", false);
+  char wifiName[] = SECRET_SSID;
+  char wifiPassword[] = SECRET_PASS;
 
-  char wifiName[20];
-  preferences.getString("wifi ssid 1", wifiName, sizeof(wifiName));
   Serial.print("WiFi ssid: ");
   Serial.println(wifiName);
-  
-  char wifiPassword[20];
-  Serial.print("WiFi password length: ");
-  Serial.println(preferences.getString("wifi pw 1", wifiPassword, sizeof(wifiPassword)));
 
+  Serial.print("ThingSpeak Channel: ");
+  Serial.println(myChannelNumber);
+    
   WiFi.begin(wifiName, wifiPassword);
 
   server.begin();
@@ -103,6 +105,8 @@ void setup() {
   bme280.begin();
 
   ccs881.begin();
+
+  ThingSpeak.begin(wifiClient);  // Initialize ThingSpeak
 
   oled.clear(ALL);
 }
@@ -157,10 +161,10 @@ void loop() {
             client.println("Content-type:text/html");
             client.println();
             client.println("<script type='text/javascript'>setInterval(function() { if (document.getElementById('auto-update').checked) { location.reload(); } }, 4000);</script>");
-            client.println("<h1>FinnPDA</h1>");
+            client.println("<h1>Air Quality Monitor</h1>");
             client.print("<p>Temperature: ");
             client.print(lastTemperature);
-            client.print(" C</p>");
+            client.print(" F</p>");
             client.print("<p>Humidity: ");
             client.print(lastHumidity);
             client.print(" %</p>");
@@ -176,7 +180,7 @@ void loop() {
             client.print("<p>Last update: ");
             client.print((millis() - lastUpdate) * 0.001);
             client.print(" s</p>");
-            client.print("<input id='auto-update' type='checkbox'>");
+            client.print("<input id='auto-update' type='checkbox' checked>");
             client.println();
             
             break;
@@ -194,7 +198,7 @@ void loop() {
     oled.clear(PAGE);
     oled.setFontType(0);
     oled.setCursor(0, 0);
-    oled.print(F("FinnPDA"));
+    oled.print(F("AirQuality"));
     
     oled.setCursor(0, 8);
     switch (WiFi.status()) {
@@ -232,7 +236,7 @@ void loop() {
     //oled.setCursor(0, 16);
     //oled.print(readRFID().substring(0, 10));
 
-    lastTemperature = bme280.readTempC();
+    lastTemperature = bme280.readTempF();
     lastHumidity = bme280.readFloatHumidity();
     lastPressure = bme280.readFloatPressure() * 0.001;
   
@@ -240,7 +244,7 @@ void loop() {
     switch (scrollingToggle % 3) {
       case 0:
         oled.print(lastTemperature);
-        oled.print(" C");
+        oled.print(" F");
         break;
       case 1:
         oled.print(lastHumidity);
@@ -260,11 +264,11 @@ void loop() {
     oled.setCursor(0, 32);
     switch (scrollingToggle % 2) {
       case 0:
-        oled.print(lastCO2);
+        oled.print(lastCO2, 0);
         oled.print(" ppm");
         break;
       case 1:
-        oled.print(lastTVOC);
+        oled.print(lastTVOC, 0);
         oled.print(" ppb");
         break;
     }
@@ -274,6 +278,20 @@ void loop() {
     scrollingToggle++;
 
     lastUpdate = millis();
+
+    // send the values to ThingSpeak
+    ThingSpeak.setField(1, lastTemperature);
+    ThingSpeak.setField(2, lastHumidity);
+    ThingSpeak.setField(3, lastPressure);
+    ThingSpeak.setField(4, lastCO2);
+    ThingSpeak.setField(5, lastTVOC);
+    int responseCode = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+    if(responseCode == 200){
+      Serial.println("Channel update successful.");
+    }
+    else{
+      Serial.println("Problem updating channel. HTTP error code " + String(responseCode));
+    }
   
     digitalWrite(LED_BUILTIN, HIGH);
     delay(100);
---
+++ AirQuality/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "YOUR SSID HERE"    // WiFi network name
+#define SECRET_PASS "YOUR WIFI PASSWORD HERE"  // WiFi password
+
+#define SECRET_CH_ID 0     // ThingSpeak channel number
+#define SECRET_WRITE_APIKEY "YOUR THINGSPEAK CHANNEL WRITE API KEY HERE"   // ThingSpeak channel write API Key
